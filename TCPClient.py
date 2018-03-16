3c7fed9872c5f9e447a9b49d195e33636b661af6
AakashBasnet <aakashbasnet@AakashBasnets-MacBook-Pro.local>
2018-03-16 12:42:25 -0400

    the name of the directory prompted for input by user

--- TCPClient.py
+++ TCPClient.py
@@ -4,9 +4,9 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 # print(clientSocket.getsockname())
-#sentence = input('Input the name of the file you want to receive: ')
-sentence = "sockets"
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print('From Server: ', modifiedSentence.decode())
+directory_search = input('Input the name of the file you want to receive: ')
+#sentence = "sockets"
+clientSocket.send(directory_search.encode())
+reply = clientSocket.recv(1024)
+print('From Server: ', reply.decode())
 clientSocket.close()
