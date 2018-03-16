4c2c23351f48d98529d2f5a077426fa6dbd2bd79
AakashBasnet <aakashbasnet@AakashBasnets-MacBook-Pro.local>
2018-03-16 12:39:25 -0400

    Name Error fixed

--- TCPServer.py
+++ TCPServer.py
@@ -2,6 +2,19 @@ import os
 from os.path import join
 from socket import *
 
+def search_for_file(filename):
+	query = filename
+	#print(query)
+	for root, dirs, files in os.walk('/Users/aakashbasnet/Desktop/'):
+		for dir_ in dirs:
+			if(query in dir_):
+				#print(query)
+				#print(dirs)
+				#print('\n')
+				return "File has been found"
+
+	return "File not found"
+
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -15,29 +28,19 @@ while 1:
 	
 	file_name = file_name.decode()
 	print("The file name is", file_name)
+	reply = search_for_file(file_name)
 	#capitalizedSentence = sentence.upper()
 	# reply = "File not Found!"
-	for root, dirs, files in os.walk('/Users/aakashbasnet/Desktop/'):
-		for dir_ in dirs:
-			print(dir_)
-			print('\n')
-			if(file_name in dir_):
-				reply = "File you requested has been found!"
+	# for root, dirs, files in os.walk('/Users/aakashbasnet/Desktop/'):
+	# 	for dir_ in dirs:
+	# 		print(dir_)
+	# 		print('\n')
+	# 		if(file_name in dir_):
+	# 			reply = "File you requested has been found!"
 
 	#reply = search_for_file(file_name)
 	connectionSocket.send(reply.encode())
 	connectionSocket.close()
 
 
-# def search_for_file(filename):
-# 	query = filename
-# 	#print(query)
-# 	for root, dirs, files in os.walk('/Users/aakashbasnet/Desktop/'):
-# 		for dir in dirs:
-# 			if(query == dir):
-# 				print(query)
-# 				print(dirs)
-# 				print('\n')
-# 				return "File found"
 
-# 	return "File not found"
